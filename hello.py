bd1fc2f68f61c923600f15f3b76eb3e7987dd010
Bryan Abbe <bmabbe@gmail.com>
2023-08-14 02:21:54 +0000

    Adding mult method

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ def add(x, y):
 
 def sub(x,y):
     return x - y
+
+def mult(x,y):
+    return x*y
     
 print(add(1, 1))
 print(sub(2, 1))
+print(mult(2,3))
--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,10 @@
-from hello import add, sub
+from hello import add, sub, mult
 
 def test_add():
     assert 2 == add(1, 1)
 
 def test_sub():
     assert 1 == sub(2, 1)
+
+def test_mult():
+    assert 6 == mult(3, 2)
